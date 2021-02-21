8ed94422ea5ae7e46eee9f5ee4940e4ea7839138
AW-AlanWu <alan.92925@gmail.com>
2021-02-21 22:49:03 +0800

    建立model，並新增model測試資料

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vocabulary.apps.VocabularyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- testsite/vocabulary/models.py
+++ testsite/vocabulary/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Words(models.Model):
+    words_text = models.CharField(max_length=50)
+    words_chinese = models.CharField(max_length=50)
+    def __str__(self):
+        return self.words_text
+
+class Sentences(models.Model):
+    words = models.ForeignKey(Words, on_delete=models.CASCADE)
+    sentences_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.sentences_text
