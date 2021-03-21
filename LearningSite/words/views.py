0fc972b3c79ce1da61859becd7015a89be8e69cc
AW-AlanWu <alan.92925@gmail.com>
2021-03-21 22:15:18 +0800

    此次開始實作view：在首頁添加了導向字集創建區的連結並利用session製作了瀏覽計數的功能;而在字集創建區的實作則不大順利，卡在單個字集單字顯示中，如何運用通用視圖，期待日後能夠解決。並更新了README

--- LearningSite/words/views.py
+++ LearningSite/words/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import CharacterSet, Vocabulary
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the words index.")
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'words/index.html', context=context)
+
+class WordEditor(generic.ListView):
+    template_name = 'words/WordEditor.html'
+    context_object_name = 'CharacterSet_list'
+
+    def get_queryset(self):
+        return CharacterSet.objects.order_by('-pk')
